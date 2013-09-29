1d078918a58e79f88638f4e63e8754f6e2bb4b4a
Seung Hwan Lee <bmipika@gmail.com>
2013-09-28 19:37:41 -0700

    Updated default message
    Updated message to say hello to hyo

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.end('Hi, Hyo\nI am running on node.js\n');
 }).listen(port);
